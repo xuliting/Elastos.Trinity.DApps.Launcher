acac9c3ad1211abb7f7430aa8833c88fcc7a9965
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-03-17 14:59:03 +0800

    Hack to solve the ion-slides width issue on ios

--- src/app/onboard/onboard.page.ts
+++ src/app/onboard/onboard.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ThemeService } from '../services/theme.service';
 import { StorageService } from '../services/storage.service';
 import { Router } from '@angular/router';
+import { IonSlides, Platform } from '@ionic/angular';
 
 declare let appManager: AppManagerPlugin.AppManager;
 
@@ -12,26 +13,53 @@ declare let appManager: AppManagerPlugin.AppManager;
 })
 export class OnboardPage implements OnInit {
 
+  @ViewChild(IonSlides) private slides: IonSlides;
+
+  hidden = true
+
+  // Slider options
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    centeredSlides: true,
+    slidesPerView: 1,
+    init:false
+  };
+
   constructor(
     public theme: ThemeService,
     private storage: StorageService,
-    private router: Router
+    private router: Router,
+    private platform: Platform
   ) { }
 
   ngOnInit() {
   }
 
   ionViewWillEnter() {
-    appManager.setVisible("show", () => {}, (err) => {});
+    appManager.setVisible("show");
   }
 
-  // slider
-  slideOpts = {
-    initialSlide: 0,
-    speed: 400,
-    centeredSlides: true,
-    slidesPerView: 1
-  };
+  ionViewDidEnter() {
+    // Dirty hack because on iOS we are currently unable to understand why the 
+    // ion-slides width is sometimes wrong when an app starts. Waiting a few 
+    // seconds (DOM fully rendered once...?) seems to solve this problem.
+    if (this.platform.platforms().indexOf('ios') >= 0) {
+      setTimeout(()=>{
+        this.showSlider();
+      }, 3000)
+    }
+    else {
+      this.showSlider();
+    }
+  }
+
+  showSlider() {
+    this.hidden = false
+    this.slides.getSwiper().then((swiper)=>{
+      swiper.init();
+    })
+  }
 
   next(slide) {
     slide.slideNext();
