ca1ee2e417adb8cf112ef7c29353d22fa056b6ec
raozhiming <raozhiming@elastos.org>
2020-05-29 08:34:06 +0800

    translate chinese

--- src/assets/languages/zh.ts
+++ src/assets/languages/zh.ts
@@ -19,20 +19,20 @@ export const zh = {
 
     // Empty Home Pg
     'no-apps-title': '没有胶囊',
-    'no-apps-message': '你可以从你的联系人或社交网络上找到elastOS capsule来浏览。你对elastOS生态系统探索得越多，你就会发现更多的胶囊。',
+    'no-apps-message': '你可以从你的联系人或社交网络上找到elastOS胶囊来浏览。你对elastOS生态系统探索得越多，你就会发现更多的胶囊。',
     'open-our-app': '你可以打开',
     'open-our-app2': '你可以打开',
     'identity-demo': '身份演示',
     'capsule-marketplace': '胶囊市场',
 
     // onboard
-    'welcome-to-a':'欢迎到一个',
-    'secure-environment':'安全的环境',
-    'introduction-0':'默认情况下，您对旧互联网的了解在elastOS中被阻止。这意味着您可以自由浏览分散的应用程序，管理独特的身份，并在安全的沙盒中拥有自己的加密钱包网络流量受限的环境。',
-    'continue':'继续',
-    'entire-ecosystem-that':'整个生态系统',
-    'we-call-capsules':'我们称之为胶囊',
-    'introduction-1':'您可以浏览自己喜欢的胶囊，从数字资产到小型网站，一应俱全。 将您的收藏夹保留在主屏幕上以便于访问，并通过查看内容来探索更多胶囊您的联系人有。',
-    'enter-elastos':'进入 elastOS',
-    
+    'welcome-to-a': '欢迎来到一个',
+    'secure-environment': '安全的环境',
+    'introduction-0': '在elastOS中，您将对旧互联网有新的理解。这意味着您可以自由浏览分散的应用程序，管理独特的身份，并在网络受限的安全的沙箱环境中拥有自己的加密钱包。',
+    'continue': '继续',
+    'entire-ecosystem-that': '整个生态系统',
+    'we-call-capsules': '我们称之为胶囊',
+    'introduction-1': '您可以浏览自己喜欢的胶囊，从数字资产到小型网站，一应俱全。 将您的收藏夹保留在主屏幕上，以方便访问，并通过查看联系人拥有的内容来探索更多胶囊。',
+    'enter-elastos': '进入 elastOS',
+
 };
